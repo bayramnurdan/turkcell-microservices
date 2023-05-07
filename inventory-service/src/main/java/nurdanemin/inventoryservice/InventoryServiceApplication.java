31ada6122590851345cbae5359567892dc8706cd
Nurdan Emin <nurdanemin0@gmail.com>
2023-05-07 19:01:48 +0300

    api getway
    api getway

--- inventory-service/src/main/java/nurdanemin/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/nurdanemin/inventoryservice/InventoryServiceApplication.java
@@ -7,11 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
 
-	}
-
+    }
 
 
 }
